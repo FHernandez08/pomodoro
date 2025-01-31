8aa3c10ccbfb0c6ca1c711d7f43c361103d3fa33
FHernandez08 <fidelhernandez_8@hotmail.com>
2025-01-31 13:51:53 -0600

    Created the countdown mechanism

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from difflib import restore
 from tkinter import *
+import math
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -13,8 +13,18 @@ LONG_BREAK_MIN = 20
 # ---------------------------- TIMER RESET ------------------------------- # 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
-
+def start_timer():
+    count_down(5 * 60)
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+def count_down(count):
+    count_min = math.floor(count / 60)
+    count_sec = count % 60
+    if count_sec < 10:
+        count_sec = f"0{count_sec}"
+
+    canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
+    if count > 0:
+        window.after(1000, count_down, count - 1)
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -26,7 +36,7 @@ title_label = Label(text="Timer", fg=GREEN, font=(FONT_NAME, 45), bg=YELLOW)
 title_label.grid(column=1, row=0)
 
 # start button
-start_btn = Button(text="Start", highlightthickness=0)
+start_btn = Button(text="Start", highlightthickness=0, command=start_timer)
 start_btn.grid(column=0, row=2)
 
 # Reset button
@@ -40,7 +50,7 @@ checkmark_label.grid(column=1, row=3)
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 image = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=image)
-canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 canvas.grid(column=1, row=1)
 
 window.mainloop()
